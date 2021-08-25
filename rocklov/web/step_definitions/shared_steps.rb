8e1065c2c25271784613640f88ac2c9bf3399e78
Bruno Souza Reis <brunosreis03@gmail.com>
2021-08-25 00:36:33 -0300

    Delete rocklov/web/step_definitions directory

--- rocklov/web/step_definitions/shared_steps.rb
+++
@@ -1,7 +0,0 @@
-Então("sou redirecionado para o Dashboard") do
-  expect(@dash_page.on_dash?).to be true # só vai ser true se tiver realmente no dashboard
-end
-
-Então("vejo a mensagem de alerta: {string}") do |expect_alert|
-  expect(@alert.dark).to eql expect_alert
-end
